16f18d2836bb067758d53816f8fc551a517600e6
Arun Udayashankar <arunkumar.u@gmail.com>
2023-05-31 00:01:44 -0400

    Turn off perl plugins; strip trailing whitespaces

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,6 +1,8 @@
 local o = vim.opt
 local g = vim.g
 
+g.loaded_perl_provider = 0  -- Turn off Perl plugins
+
 -- Indentation
 o.smartindent = true
 o.tabstop = 2
@@ -21,8 +23,21 @@ o.number = true
 
 -- Misc
 o.ignorecase = true
+o.incsearch = true
+o.hlsearch = true
 o.wrap = false
 o.backup = false
 o.writebackup = false
 o.swapfile = false
 o.errorbells = false
+
+
+-- Trailing Whitespaces Vacuum Cleaner
+function vim.fn.stripTrailingWhitespace()
+    local l = vim.fn.line(".")
+    local c = vim.fn.col(".")
+    vim.cmd("%s/\\s\\+$//e")
+    vim.fn.cursor(l, c)
+end
+
+vim.cmd("autocmd BufWritePre * :lua vim.fn.stripTrailingWhitespace()")   -- strip all files by default
