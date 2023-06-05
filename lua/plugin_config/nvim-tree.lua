e95cc055a6ee4894960f72636372f82b665fe9bc
Arun Udayashankar <arunkumar.u@gmail.com>
2023-06-04 22:59:22 -0400

    Update nvim-tree config

--- lua/plugin_config/nvim-tree.lua
+++ lua/plugin_config/nvim-tree.lua
@@ -7,7 +7,36 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 vim.opt.termguicolors = true
+vim.keymap.set('n', '<C-.>', ':NvimTreeToggle<CR>')
 
-nvim_tree.setup()
+local function nvim_tree_on_attach(bufnr)
+  local api = require "nvim-tree.api"
+  local function opts(desc)
+    return {
+      desc = "nvim-tree: " .. desc,
+      buffer = bufnr, noremap = true, silent = true, nowait = true
+    }
+  end
 
-vim.keymap.set('n', '<c-.>', ':NvimTreeFindFileToggle<CR>')
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
+
+  -- custom mappings
+  vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
+  vim.keymap.set('n', 'h', api.tree.toggle_help, opts('Help'))
+end
+
+nvim_tree.setup {
+  on_attach = nvim_tree_on_attach,
+
+  sort_by = "case_sensitive",
+  view = {
+    width = 30,
+    mappings = {
+      custom_only = false,
+    }
+  },
+  filters = {
+    dotfiles = true,
+  }
+}
