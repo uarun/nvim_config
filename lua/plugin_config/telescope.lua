ea75816bc21c02e27a0cf5719c1555d6c45b8272
Arun Udayashankar <arunkumar.u@gmail.com>
2024-03-02 19:34:37 -0500

    Add Telescope fzf configuration

--- lua/plugin_config/telescope.lua
+++ lua/plugin_config/telescope.lua
@@ -14,5 +14,15 @@ keymap('n', '<leader>fg', builtin.live_grep, opts)
 keymap('n', '<leader>fb', builtin.buffers, opts)
 keymap('n', '<leader>fh', builtin.help_tags, opts)
 
-telescope.setup({})
+telescope.setup {
+  extensions = {
+    fzf = {
+      fuzzy = true,                    -- false will only do exact matching
+      override_generic_sorter = true,  -- override the generic sorter
+      override_file_sorter = true,     -- override the file sorter
+      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+                                       -- the default case_mode is "smart_case"
+    }
+  }
+}
 telescope.load_extension("fzf")
